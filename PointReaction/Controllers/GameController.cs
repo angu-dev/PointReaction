ffb55806e828e127b26d51a512f320743ea52c4c
Andreas Gutmann <andreas.gutmann01@gmail.de>
2020-10-27 07:07:41 +0100

    Grundentwurf der Seiten und Verwaltung dieser hinzugefügt

---
+++ PointReaction/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointReaction.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Leaderboard()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
---
+++ PointReaction/Controllers/VerifyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointReaction.Controllers
+{
+    public class VerifyController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
