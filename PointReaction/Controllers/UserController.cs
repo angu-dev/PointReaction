199760adc67c0155155c990819a6c0c35709eec0
Andreas Gutmann <andreas.gutmann01@gmail.de>
2020-10-27 15:53:50 +0100

    Verzeichnisse erstellt, Einstellungsweiterleitung hinzugefügt

---
+++ PointReaction/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointReaction.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
